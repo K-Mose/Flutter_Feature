8ac379f40664aac84eef5094b74393b6dbc9d7f3
Mose <km_413@naver.com>
2024-12-26 14:18:25 +0900

    GetX Test - Feature: 1. init project

---
+++ get_x_test/README.md
@@ -0,0 +1,5 @@
+# get_x_test
+[GetX](https://pub.dev/packages/get#the-three-pillars)
+## GetX Test
+- State Management
+- Routing
---
+++ get_x_test/android/app/src/main/kotlin/com/mose/get_x_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.mose.get_x_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
